34825ca21a90333a44a11c7a7710465105d63a4f
Kurtney Joseph <josephkurt18@gmail.com>
2023-09-16 23:44:28 -0400

    feat: add toggle

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,24 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
-const Root: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const Root: React.FC<{
+  children: React.ReactNode;
+  setDrop: any;
+  isDropOpen: boolean;
+}> = ({ children, setDrop, isDropOpen }) => {
   const themeMode = useContext(ThemeContext);
+
+  const handleDrop = () => {
+    if (isDropOpen) {
+      setDrop(false);
+    }
+  };
+
   return (
     <div
       className={`${
         themeMode!.theme === 'dark' ? 'bg-lightblk' : 'bg-white'
       } h-screen`}
+      onClick={handleDrop}
     >
       {children}
     </div>
