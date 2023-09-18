647811b31b7b70bc8c5af9ee41efd15d243e4b65
Kurtney Joseph <josephkurt18@gmail.com>
2023-09-17 23:17:24 -0400

    refactor: context wrapping

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { Header } from './components/Header';
 import { FontProvider } from './contexts/FontContext';
 import { Root } from './Root';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { Layout } from './components/Layout';
+import { DataProvider } from './contexts/DataContext';
 
 const App: React.FC<{}> = () => {
   const [isDropdownOpen, setDropdown] = useState<boolean>(false);
   return (
     <ThemeProvider>
       <Root setDrop={setDropdown} isDropOpen={isDropdownOpen}>
-        <FontProvider>
-          <Header isDropopen={isDropdownOpen} setDrop={setDropdown} />
-        </FontProvider>
+        <DataProvider>
+          <FontProvider>
+            <Header isDropopen={isDropdownOpen} setDrop={setDropdown} />
+            <Layout />
+          </FontProvider>
+        </DataProvider>
       </Root>
     </ThemeProvider>
   );
