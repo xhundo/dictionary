128e35a7be372c7650aaa7c77ed3bb74919e630c
Kurtney Joseph <josephkurt18@gmail.com>
2023-09-18 23:36:30 -0400

    feat: layout audio && definition

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useMemo } from 'react';
 import { Howl } from 'howler';
 import { FontContext } from '../contexts/FontContext';
 import { ThemeContext } from '../contexts/ThemeContext';
@@ -9,20 +9,28 @@ import { PlayButton } from './PlayButton';
 const Layout: React.FC = () => {
   const { theme } = useContext(ThemeContext)!;
   const { font } = useContext(FontContext)!;
-  let { definition } = useContext(DataContext)!;
+  let { definition: data, setQuery, errors } = useContext(DataContext)!;
 
   const showLayout = () => {
-    if (definition) {
+    if (data && !errors) {
       const pick =
         font === 'Sans Serif'
-          ? font.trim().slice(3).replace('s', '').toLowerCase()
+          ? font.toLowerCase().trim().replace(' ', '')
           : font.toLowerCase();
 
+      const definition = {
+        word: data.word,
+        phonetic: data.phonetic,
+        audio: data?.phonetics[0]?.audio,
+        pos: data.meanings[0].partOfSpeech
+      };
+
+      console.log(data, errors);
+
       const audio = new Howl({
-        src: [definition?.phonetics[0]?.audio]
+        src: [definition.audio]
       });
 
-      console.log(definition);
       return (
         <div className="relative mt-[43px]">
           <section className="mb-10 flex justify-between">
@@ -38,8 +46,31 @@ const Layout: React.FC = () => {
             </div>
             <PlayButton action={() => audio.play()} />
           </section>
+          <main className="flex flex-col">
+            <div className="flex gap-5">
+              <p
+                className={`font-${pick} text-2xl ${
+                  pick === 'sansserif' && 'font-bold italic'
+                }  ${theme === 'dark' ? 'text-white' : 'text-dark-grayish'}`}
+              >
+                {definition.pos}
+              </p>
+              <span className="bg-span h-[1px] w-full self-center"></span>
+            </div>
+            <p
+              className={`font-${pick} mt-[38px] text-xl font-normal text-grayish`}
+            >
+              Meaning
+            </p>
+          </main>
         </div>
       );
+    } else {
+      if (errors) {
+        let { title, message, resolution } = errors;
+        console.log(title, message, resolution);
+      }
+      return <div></div>;
     }
   };
 
@@ -50,4 +81,5 @@ const Layout: React.FC = () => {
     </main>
   );
 };
+
 export { Layout };
