d32b92af5977df0e663a42eccca6e90731ded916
Kurtney Joseph <josephkurt18@gmail.com>
2023-09-20 23:16:35 -0400

    refactor: layout build

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,41 +1,63 @@
 import React, { useState, useEffect, useContext, useMemo } from 'react';
+import { Definition } from '../interfaces';
 import { Howl } from 'howler';
-import { v4 as uuidv4 } from 'uuid';
+import _ from 'lodash';
+import { setPick } from '../utils/helper';
 import { FontContext } from '../contexts/FontContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { DataContext } from '../contexts/DataContext';
 import { SearchBar } from './SearchBar';
 import { PlayButton } from './PlayButton';
 
-const Layout: React.FC = () => {
+const Layout: React.FC<{}> = ({}) => {
   const { theme } = useContext(ThemeContext)!;
   const { font } = useContext(FontContext)!;
   let { definition: data, setQuery, errors } = useContext(DataContext)!;
 
+  const pick = setPick(font);
+  let definition: Definition;
+
   const showLayout = () => {
     if (data && !errors) {
-      const pick =
-        font === 'Sans Serif'
-          ? font.toLowerCase().trim().replace(' ', '')
-          : font.toLowerCase();
+      console.log(data);
 
-      const definition = {
+      definition = {
         word: data.word,
         phonetic: data.phonetic,
-        audio: data?.phonetics[0]?.audio,
-        pos: data.meanings[0].partOfSpeech,
-        pos_two: data?.meanings[1]?.partOfSpeech,
-        meaning: data.meanings,
+        audio: data.phonetics[0]?.audio,
+        pos_noun: data.meanings[0].partOfSpeech,
+        pos_verb: data.meanings[1]?.partOfSpeech,
+        meaning_one: data.meanings[0],
+        meaning_two: data.meanings[1],
         synonyms: data.meanings[0].synonyms,
-        example: data?.meanings[1]?.definitions[0].example
+        example: data.meanings[1]?.definitions[0].example
       };
-
-      console.log(data.meanings);
-      console.log(data, errors);
 
       const audio = new Howl({
         src: [definition.audio]
       });
+
+      console.log('meaning: ', definition.meaning_one, definition.meaning_two);
+
+      const renderMarkup = (): JSX.Element[] => {
+        return definition.meaning_one.definitions.map(
+          ({ definition }: { definition: string }) => {
+            return (
+              <div>
+                <ol key={_.uniqueId()} className="ml-[22px] list-outside">
+                  <li
+                    className={`mb-3 list-disc ${
+                      theme === 'dark' ? 'text-white' : 'text-dark-grayish'
+                    } leading-6  marker:text-purple font-${pick} text-xl font-normal`}
+                  >
+                    {definition}
+                  </li>
+                </ol>
+              </div>
+            );
+          }
+        );
+      };
 
       return (
         <div className="relative mt-[43px]">
@@ -59,7 +81,7 @@ const Layout: React.FC = () => {
                   pick === 'sansserif' && 'font-bold italic'
                 }  ${theme === 'dark' ? 'text-white' : 'text-dark-grayish'}`}
               >
-                {definition.pos}
+                {definition.pos_noun}
               </p>
               <span className="h-[1px] w-full self-center bg-span"></span>
             </div>
@@ -68,45 +90,7 @@ const Layout: React.FC = () => {
             >
               Meaning
             </p>
-            <section className="mb-63 pl-[22px]">
-              {definition.meaning.length > 1
-                ? definition.meaning[0].definitions.map(
-                    ({ definition }: { definition: string[] }) => {
-                      return (
-                        <div>
-                          <ol key={uuidv4()} className="ml-[22px] list-outside">
-                            <li
-                              className={`mb-3 list-disc ${
-                                theme === 'dark'
-                                  ? 'text-white'
-                                  : 'text-dark-grayish'
-                              } leading-6  marker:text-purple font-${pick} text-xl font-normal`}
-                            >
-                              {definition}
-                            </li>
-                          </ol>
-                        </div>
-                      );
-                    }
-                  )
-                : definition.meaning[0].definitions.map(
-                    ({ definition }: { definition: string }) => {
-                      return (
-                        <ol key={uuidv4()} className="ml-[22px] list-outside">
-                          <li
-                            className={`mb-3 list-disc ${
-                              theme === 'dark'
-                                ? 'text-white'
-                                : 'text-dark-grayish'
-                            } leading-6  marker:text-purple font-${pick} text-xl font-normal`}
-                          >
-                            {definition}
-                          </li>
-                        </ol>
-                      );
-                    }
-                  )}
-            </section>
+            <section className="mb-63 pl-[22px]">{renderMarkup()}</section>
             <section className="mb-[53px] flex gap-6">
               <p className={`font-${font} text-xl font-normal text-grayish`}>
                 Synonyms
@@ -115,9 +99,9 @@ const Layout: React.FC = () => {
                 {definition.synonyms[0]}
               </p>
             </section>
-            {definition.meaning.length > 1 ? (
-              definition.meaning[1].definitions.map(
-                ({ definition: def }: { definition: string }) => {
+            {data.meanings.length > 1 ? (
+              definition.meaning_two.definitions
+                .map(({ definition: def }: { definition: string }) => {
                   return (
                     <>
                       <div className="mb-10 flex gap-5">
@@ -130,18 +114,16 @@ const Layout: React.FC = () => {
                               : 'text-dark-grayish'
                           }`}
                         >
-                          {definition.pos_two}
+                          {definition.pos_verb}
                         </p>
-
                         <span className="h-[1px] w-full self-center bg-span"></span>
                       </div>
-
                       <p
                         className={`font-${pick} mb-6 text-xl font-normal text-grayish`}
                       >
                         Meaning
                       </p>
-                      <ol key={uuidv4()} className="ml-[22px] list-disc ">
+                      <ol key={_.uniqueId()} className="ml-[22px] list-disc">
                         <li
                           className={`mb-3 ml-[22px]  list-outside ${
                             theme === 'dark'
@@ -152,17 +134,20 @@ const Layout: React.FC = () => {
                           {def}
                         </li>
                       </ol>
-                      {definition.example && (
-                        <p
-                          className={`ml-[22px] text-lg text-grayish font-${pick} font-normal`}
-                        >
-                          "{definition.example}"
-                        </p>
-                      )}
+                      <div className="mb-10">
+                        {definition.example && (
+                          <p
+                            className={`ml-[22px] text-lg text-grayish font-${pick} font-normal`}
+                          >
+                            "{definition.example}"
+                          </p>
+                        )}
+                      </div>
+                      <span className="h-[1px] w-full self-center bg-span"></span>
                     </>
                   );
-                }
-              )
+                })
+                .slice(0, 1)
             ) : (
               <></>
             )}
@@ -179,7 +164,7 @@ const Layout: React.FC = () => {
   };
 
   return (
-    <main className="mt-59 flex min-h-screen w-1/2 flex-col self-center">
+    <main className="mt-59 flex min-h-full w-1/2 flex-col self-center">
       <SearchBar />
       {showLayout()}
     </main>
